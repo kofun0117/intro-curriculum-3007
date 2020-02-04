4161999530004a4d731512fb4de7061a0e989f93
kofun0117 <ocean_pv@yahoo.co.jp>
2020-02-04 23:20:51 +0900

    sMultipleOfSeventeen 関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
-
+function isMultipleOfSeventeen(num){
+    let check_result = false;
+    if (num%17 === 0) {
+        check_result = true;
+    }
+    return check_result;
+}
 module.exports = {
+    isMultipleOfSeventeen
 }
